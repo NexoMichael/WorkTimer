84e5ec2e37ea6c9a9fa260ba718cb80164b31b36
Michael Kochegarov <nexo.michael@gmail.com>
2021-03-17 21:33:25 +0100

    Initial Commit

---
+++ WorkTimer/WorkTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkTimerApp.swift
+//  WorkTimer
+//
+//  Created by Michael Kochegarov on 17.03.21.
+//
+
+import SwiftUI
+
+@main
+struct WorkTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
